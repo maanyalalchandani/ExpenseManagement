180fd47b4727453ebb28178cae7c976092073af6
Maanya Lalchandani <maanyalalchandani@gmail.com>
2024-06-06 15:00:48 +0530

    Background Image Modified

--- src/components/Auth/AuthLayout.js
+++ src/components/Auth/AuthLayout.js
@@ -4,10 +4,12 @@ const AuthLayout = ({ title, subtitle, children }) => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="flex flex-col lg:flex-row w-full max-w-4xl bg-white rounded-lg shadow-lg overflow-hidden">
-        <div className="flex flex-col justify-center items-center w-full lg:w-1/2 bg-blue-500 text-white p-8">
-          <h1 className="text-3xl font-bold mb-4">{title}</h1>
-          <p className="text-lg">{subtitle}</p>
-          <img src="publ\images\bg_login.jpeg" alt="Decoration" className="mt-4 w-full h-56 object-cover lg:hidden" />
+        <div className="relative flex flex-col justify-center items-center w-full lg:w-1/2 p-8 bg-blue-500 text-white">
+          <div className="absolute inset-0 bg-cover bg-center opacity-100" style={{ backgroundImage: `url('/images/bg_login.jpeg')` }}></div>
+          <div className="relative z-10">
+            <h1 className="text-3xl font-bold mb-4">{title}</h1>
+            <p className="text-lg">{subtitle}</p>
+          </div>
         </div>
         <div className="w-full lg:w-1/2 p-8 flex flex-col justify-center">{children}</div>
       </div>
