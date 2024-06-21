0f22aef9953f43f3a59b2ed34fe31727e104a0fe
Maanya Lalchandani <maanyalalchandani@gmail.com>
2024-06-21 23:26:39 +0530

    email utility

--- src/components/utils/emailUtils.js
+++ src/components/utils/emailUtils.js
@@ -1,8 +1,22 @@
-export const sendExpenseEmail = (expense) => {
-    // Placeholder function to simulate sending an email
-    console.log('Sending email with the following expense details:');
-    console.log(`Date: ${expense.date}`);
-    console.log(`Category: ${expense.category}`);
-    console.log(`Amount: ${expense.amount}`);
+import emailjs from 'emailjs-com';
+
+const SERVICE_ID = 'your_service_id';
+const TEMPLATE_ID = 'your_template_id';
+const USER_ID = 'your_user_id';
+
+export const sendExpenseEmail = (expense, recipientEmail) => {
+  const templateParams = {
+    to_email: recipientEmail,
+    date: expense.date,
+    category: expense.category,
+    amount: expense.amount,
   };
-  
+
+  emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, USER_ID)
+    .then((response) => {
+      console.log('Email successfully sent!', response.status, response.text);
+    })
+    .catch((err) => {
+      console.error('There was an error sending the email:', err);
+    });
+};
